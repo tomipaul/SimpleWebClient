51f020e2dbc22aa2e37b806d4378cd879b4d2d26
Oluwatomi Akande <tomipaul95@gmail.com>
2016-08-23 02:04:48 +0100

    Output JSON Object obtained from API in  form of formatted strings and add the .gitignore file

--- SimpleWebClient.js
+++ SimpleWebClient.js
@@ -6,21 +6,28 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('Welcome to Web client\nPlease enter your Medium integration token', (answer) => {
-	var authTokens = answer;
+
+
+rl.question('Welcome to Web client\nPlease enter your Medium integration token\n', (answer) => {
+    authTokens = answer;
     request({
         url: 'https://api.medium.com/v1/me', //URL to hit
         method: 'GET', //Specify the method
         headers: { //We can define headers too
-            'Content-Type': 'MyContentType',
+            'Content-Type': 'application/json',
             'Authorization': 'Bearer '+authTokens,
-            'Custom-Header': 'Custom Value'
+            'Custom-Header': 'Custom Value',
+            'Accept': 'application/json',
+            'Accept-Charset': 'utf-8'
         }
     }, function(error, response, body){
         if(error) {
             console.log(error);
         } else {
-            console.log(response.statusCode, body);
+            var obj = JSON.parse(body);
+            userId = obj.data.id;
+            console.log('\nName: '+obj.data.name+'\nUsername: '+obj.data.username+'\nURL: '+obj.data.url);
+            console.log('Response status code: ',response.statusCode);
         }
     });
     rl.close();
